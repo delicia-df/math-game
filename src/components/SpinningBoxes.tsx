e47a6ae314c317350eca188309c5ae13d09d4824
delicia-df <deliciadryfrt@gmail.com>
2024-12-06 14:43:46 +0530

    Add react three textures

--- src/components/SpinningBoxes.tsx
+++ src/components/SpinningBoxes.tsx
@@ -4,6 +4,8 @@ import { OrbitControls } from '@react-three/drei';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import fontData from 'three/examples/fonts/helvetiker_bold.typeface.json';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { useTexture } from "@react-three/drei"
+
 interface BoxProps {
     position: [number, number, number];
     text: string;
@@ -42,6 +44,20 @@ const getTextWidth = (text: string, font) =>{
 
 const Box: React.FC<BoxProps> = ({ position, text, font, speed }) => {
     const meshRef = useRef<any>(null);
+    // const props = useTexture({
+    //     map: 'PavingStones092_1K_Color.jpg',
+    //     // displacementMap: 'PavingStones092_1K_Displacement.jpg',
+    //     normalMap: 'PavingStones092_1K_Normal.jpg',
+    //     roughnessMap: 'PavingStones092_1K_Roughness.jpg',
+    //     aoMap: 'PavingStones092_1K_AmbientOcclusion.jpg',
+    //   })
+      const props = useTexture({
+        map: 'textures/hangar_concrete_floor_diff_1k.jpg',
+        // displacementMap: 'textures/wood_floor_disp_1k.jpg',
+        normalMap: 'textures/hangar_concrete_floor_nor_dx_1k.jpg',
+        roughnessMap: 'textures/hangar_concrete_floor_rough_1k.jpg',
+        aoMap: 'textures/hangar_concrete_floor_ao_1k.jpg',
+      })
     useFrame(() => {
         const rotationSpeed= speed*0.0005
         meshRef.current.rotation.x += rotationSpeed;
@@ -58,15 +74,19 @@ const Box: React.FC<BoxProps> = ({ position, text, font, speed }) => {
     const textWidth = textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x;
     console.log(textGeometry.parameters.options, textWidth)
     const boxWidth = textWidth + 1; // Add 1 unit of padding
+    console.log("texture",props)
     return (
         <mesh ref={meshRef} position={position}>
             <boxGeometry args={[boxWidth, 2, 0.5]} />
-            <meshPhongMaterial color={'white'} />
-            <mesh position={[-textWidth / 2, -0.3, 0.25]}>
+            <meshPhongMaterial color={"white"} />
+            <meshPhongMaterial color={"white"} {...props}  />
+            <mesh position={[-textWidth / 2, -0.4, 0.25]}>
                 <primitive object={textGeometry} />
-                <meshPhongMaterial color={'black'} />
+                <meshPhongMaterial color={'white'} />
             </mesh>
         </mesh>
+     
+
     );
 };
 
@@ -74,7 +94,7 @@ const SpinningBoxes = ({textArray,speed}:{textArray:string[],speed:number}) => {
     const font = new FontLoader().parse(fontData);
     const xOffsets = sumBelowIndex(textArray.map(text => getTextWidth(text, font)))
     return (
-        <div style={{ background: "gray" }}>
+        <div style={{ background: "white" }}>
             <Canvas style={{width: xOffsets[xOffsets.length-1]*40, height: 100}} camera={{ position: [0, 0, 4] }}>
                 <ambientLight intensity={10} />
                 <pointLight position={[0, 0, 5]} intensity={0.5} />
--- src/lib/math.ts
+++ src/lib/math.ts
@@ -3,13 +3,12 @@ export function generateQuestion(level: number): {
   question: string[];
   answer: number;
 } {
-  const operations = ['+', '-', '*', '/'];
+  const operations = ['+', '-', '*'];
   const opSize = operations.length; 
   const operation = operations[Math.floor(Math.random() * 10) %opSize];
   
-  const maxNum = Math.min(12, 3 + level * 2);
-  const num1 = Math.floor(Math.random() * maxNum) + 1;
-  const num2 = Math.floor(Math.random() * maxNum) + 1;
+  const num1 = Math.floor(Math.random() * 10**level) + 1;
+  const num2 = Math.floor(Math.random() * 10**level) + 1;
   
   let answer: number;
   switch (operation) {
